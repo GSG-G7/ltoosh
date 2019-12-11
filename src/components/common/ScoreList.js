91b861da7c3763a8faad5c91e4cfe10b9e148595
amoodaa <amoodaa@gmail.com>
2019-12-11 11:20:48 +0200

    clickers left

--- src/components/common/ScoreList.js
+++ src/components/common/ScoreList.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Text, View, StyleSheet, TextInput } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Fontisto';
 import { updatePlayerScoreAction } from '../../redux/actions';
 
 const styles = StyleSheet.create({
-  container: { flex: 1, flexDirection: 'row' },
-  oneList: { flex: 1, justifyContent: 'flex-start', alignItems: 'center' },
+  container: { flex: 1 },
+  header: { flex: 1, flexDirection: 'row' },
+  scoresContainer: { flex: 12 },
   playerName: {
     flex: 1,
     justifyContent: 'center',
@@ -26,6 +27,8 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
     borderBottomWidth: 0.5,
   },
+  oneRow: { flex: 1, flexDirection: 'row' },
+  // container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
 });
 const Card = ({ text }) => (
   <View style={styles.card}>
@@ -43,7 +46,7 @@ const Clicker = ({ onSubmit }) => {
       }}>
       <TextInput
         style={{ flex: 1, fontSize: 20 }}
-        defaultValue={`${count}`}
+        defaultValue="0"
         onChangeText={txt => setCount(+txt)}
         keyboardType="number-pad"
       />
@@ -54,39 +57,84 @@ const Clicker = ({ onSubmit }) => {
     </View>
   );
 };
-const OneList = ({ playerName, scoreArr, updatePlayerScore }) => (
-  <View style={styles.oneList}>
-    <View style={styles.playerName}>
-      <Text style={{ fontSize: styles.playerName.fontSize }}>{playerName}</Text>
-    </View>
-    <View style={styles.scoreList}>
-      <FlatList
-        keyboardShouldPersistTaps="always"
-        data={scoreArr}
-        renderItem={({ item }) => <Card text={item} />}
-        keyExtractor={(e, i) => `${e}${i}`}
-      />
-    </View>
-    <Clicker onSubmit={updatePlayerScore} />
-  </View>
-);
-const ScoreList = ({ scores, updatePlayerScore }) => (
-  <View style={styles.container}>
-    {Object.entries(scores).map(([playerName, scoreArr]) => (
-      <OneList
-        key={playerName}
-        playerName={playerName}
-        scoreArr={scoreArr}
-        updatePlayerScore={ammount => updatePlayerScore({ name: playerName, scoreChange: ammount })}
-      />
+const ScoreListHeader = ({ playerNames }) => (
+  <View style={styles.header}>
+    {playerNames.map(playerName => (
+      <View style={styles.playerName}>
+        <Text style={{ fontSize: styles.playerName.fontSize }}>{playerName}</Text>
+      </View>
     ))}
   </View>
 );
 
-const mapStateToProps = ({ scores }) => ({
-  scores,
+const ScoresScrollView = ({ scores }) => {
+  const formatList = lists => {
+    const max = lists.reduce((acum, arr) => Math.max(arr.length, acum), 0);
+    const formatted = new Array(max).fill([]).map(() => new Array(scores.length).fill(0));
+    lists.forEach((list, inner) =>
+      list.forEach((e, outer) => {
+        if (!formatted[outer]) formatted[outer] = [];
+        formatted[outer][inner] = e;
+      })
+    );
+    return formatted;
+  };
+  return (
+    <View style={styles.scoresContainer}>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.scoresContainer}>
+          {formatList(scores).map((row, i) => (
+            <View style={styles.oneRow}>
+              {row.map((
+                text,
+                j // [10, 2, 2, 2, 2]
+              ) => (
+                <Card key={`Card(${i + 1},${j + 1})`} text={text} />
+              ))}
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+const ScoreList = ({ playerNames, scores, updatePlayerScore }) => (
+  <View style={styles.container}>
+    <ScoreListHeader playerNames={playerNames} />
+    <ScoresScrollView scores={scores} />
+  </View>
+);
+const mapStateToProps = ({ scores: data }) => ({
+  playerNames: Object.keys(data),
+  scores: Object.values(data),
 });
 
 const mapDispatchToProps = { updatePlayerScore: updatePlayerScoreAction };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ScoreList);
+
+/* {Object.entries(scores).map(([playerName, scoreArr]) => (
+      <OneList
+        key={playerName}
+        playerName={playerName}
+        scoreArr={scoreArr}
+        updatePlayerScore={ammount => updatePlayerScore({ name: playerName, scoreChange: ammount })}
+        />
+        ))} */
+// const OneList = ({ playerName, scoreArr, updatePlayerScore }) => (
+//   // oneList: { flex: 1, justifyContent: 'flex-start', alignItems: 'center' },
+//   <View style={styles.oneList}>
+//     <View style={styles.playerName}>
+//       <Text style={{ fontSize: styles.playerName.fontSize }}>{playerName}</Text>
+//     </View>
+//     <View style={styles.scoreList}>
+//       <FlatList
+//         keyboardShouldPersistTaps="always"
+//         data={scoreArr}
+//         renderItem={({ item }) => <Card text={item} />}
+//         keyExtractor={(e, i) => `${e}${i}`}
+//       />
+//     </View>
+//     <Clicker onSubmit={updatePlayerScore} />
+//   </View>
+// );
